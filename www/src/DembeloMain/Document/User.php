f74e96dbfd90adcd3c506e23b6cd99d249c81a7f
skreutzer <stephankreutzer@gmx.net>
2015-08-13 18:30:13 +0200

    Issue #55: Read topics from the database.

--- www/src/DembeloMain/Document/User.php
+++ www/src/DembeloMain/Document/User.php
@@ -1,6 +1,6 @@
 <?php
 
-/* Copyright (C) 2015 Michael Giesler
+/* Copyright (C) 2015 Michael Giesler, Stephan Kreutzer
  *
  * This file is part of Dembelo.
  *
@@ -61,6 +61,11 @@ class User implements UserInterface, \Serializable
      * @Assert\NotBlank()
      */
     protected $roles;
+
+    /**
+     * @MongoDB\Collection
+     */
+    protected $currentTextnodes;
 
     /**
      * gets the mongodb id
@@ -157,6 +162,20 @@ class User implements UserInterface, \Serializable
         $this->roles = $roles;
     }
 
+    public function getCurrentTextnode($themeId)
+    {
+        if (isset($this->currentTextnodes[$themeId]) === true) {
+            return $this->currentTextnodes[$themeId];
+        }
+
+        return null;
+    }
+
+    public function setCurrentTextnode($themeId, $textnodeId)
+    {
+        $this->currentTextnodes[$themeId] = $textnodeId;
+    }
+
     /**
      * from UserInterface
      */
@@ -177,6 +196,7 @@ class User implements UserInterface, \Serializable
             $this->password,
             // see section on salt below
             // $this->salt,
+            $this->currentTextnodes
         ));
     }
 
@@ -192,7 +212,8 @@ class User implements UserInterface, \Serializable
             $this->email,
             $this->password,
             // see section on salt below
-            // $this->salt
+            // $this->salt,
+            $this->currentTextnodes
             ) = unserialize($serialized);
     }
 }
