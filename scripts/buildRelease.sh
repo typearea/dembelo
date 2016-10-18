2f7f13278086bedadf1d9262df072c4af2d4d635
Michael Giesler <michael@4horsemen.de>
2016-10-18 18:50:54 +0200

    issue #233, attempt to build release in travis

--- scripts/buildRelease.sh
+++ scripts/buildRelease.sh
@@ -2,8 +2,9 @@
 export SYMFONY_ENV=prod
 mkdir release
 cd release
-git checkout git@github.com:typearea/dembelo.git
-composer --working-dir="www" remove alcaeus/mongo-php-adapter mongodb/mongodb
+git clone git@github.com:typearea/dembelo.git
+cd dembelo
+composer remove alcaeus/mongo-php-adapter mongodb/mongodb
 composer install --no-dev --optimize-autoloader
 rm -r .git
 echo "zip release: ", $RELEASE_FILENAME
