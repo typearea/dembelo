d4b8b7f78c7693f283a8f2b524968bb93f4759ad
Michael Giesler <michael@4horsemen.de>
2016-10-18 21:54:14 +0200

    issue #233, attempt to build release in travis

--- scripts/buildRelease.sh
+++ scripts/buildRelease.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 mkdir release
-#echo -n $DEMBELO_VERSION > files/version
-#git add files/version
-#git commit -m "Set build version number" public/version
-#git tag $DEMBELO_VERSION -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
-#git push --quiet https://$GITHUBKEY@github.com/typearea/dembelo $DEMBELO_VERSION > /dev/null 2>&1
+echo -n $DEMBELO_VERSION > files/version
+git add files/version
+git commit -m "Set build version number" public/version
+git tag $DEMBELO_VERSION -a -m "Generated tag from TravisCI build $TRAVIS_BUILD_NUMBER"
+git push --quiet git@github.com:typearea/dembelo.git $DEMBELO_VERSION
 git checkout-index -a -f --prefix release/
 cd release
 export SYMFONY_ENV=prod
