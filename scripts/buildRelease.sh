d22dfe2c13156decc6a4c0c1f2e88180f50ebe1d
Michael Giesler <michael@4horsemen.de>
2016-10-18 19:24:46 +0200

    issue #233, attempt to build release in travis

--- scripts/buildRelease.sh
+++ scripts/buildRelease.sh
@@ -6,5 +6,5 @@ cd release
 composer --working-dir="www" remove alcaeus/mongo-php-adapter mongodb/mongodb
 composer --working-dir="www" install --no-dev --optimize-autoloader
 echo "zip release: ", $RELEASE_FILENAME
-zip -r $RELEASE_FILENAME ./
+zip -rq $RELEASE_FILENAME ./
 ls -l $RELEASE_FILENAME
