b232eef3191b449879c8e0d4259633d45b889682
Michael Giesler <michael@4horsemen.de>
2016-10-18 18:19:21 +0200

    issue #233, attempt to build release in travis

--- scripts/buildRelease.sh
+++ scripts/buildRelease.sh
@@ -6,4 +6,6 @@ git checkout git@github.com:typearea/dembelo.git
 composer --working-dir="www" remove alcaeus/mongo-php-adapter mongodb/mongodb
 composer install --no-dev --optimize-autoloader
 rm -r .git
+echo "zip release: ", $RELEASE_FILENAME
 zip -r $RELEASE_FILENAME ./
+ls -l $RELEASE_FILENAME
