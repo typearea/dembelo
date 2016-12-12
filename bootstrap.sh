a45e376ef508518ae1360d30d0de773f6275023c
Michael Giesler <michael@4horsemen.de>
2016-12-12 20:59:56 +0100

    issue #203, debugging visibility issue in travis

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,6 +17,8 @@ composer self-update
 
 gem install sass
 
+ls -l vendor/bundle/
+
 cp /vagrant/files/nginx/default /etc/nginx/sites-available/default
 ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 
