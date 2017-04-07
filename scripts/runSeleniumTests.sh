50fa6b9b9074600ac156002d911f7e7b206364cf
Michael Giesler <michael@4horsemen.de>
2017-04-07 23:04:34 +0200

    change  path to console

--- scripts/runSeleniumTests.sh
+++ scripts/runSeleniumTests.sh
@@ -2,15 +2,15 @@
 
 cd ./www/
 
-php app/console server:start -v
+php bin/console server:start -v
 bin/phantomjs --webdriver=8910 --webdriver-loglevel=ERROR &
-app/console assetic:dump --env=selenium
-app/console cache:warmup --env=selenium -q
+bin/console assetic:dump --env=selenium
+bin/console cache:warmup --env=selenium -q
 
 bin/phpunit -c app/phpunitselenium.xml
 RETURNVALUEPHPUNIT=$?
 
 pkill -f 'bin/phantomjs --webdriver=8910'
-php app/console server:stop -q
+php bin/console server:stop -q
 
 exit $RETURNVALUEPHPUNIT
