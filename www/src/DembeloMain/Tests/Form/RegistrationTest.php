1dcd14376e6b814de2f218213a4abf2c085b8352
Michael Giesler <michael@4horsemen.de>
2018-02-25 22:20:30 +0100

    issue #451: add Unittests for new Form classes

---
+++ www/src/DembeloMain/Tests/Form/RegistrationTest.php
@@ -0,0 +1,45 @@
+<?php
+/* Copyright (C) 2018 Michael Giesler
+ *
+ * This file is part of Dembelo.
+ *
+ * Dembelo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Dembelo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License 3 for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License 3
+ * along with Dembelo. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace DembeloMain\Tests\Form;
+
+use DembeloMain\Form\Registration;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * Class LoginTest
+ */
+class RegistrationTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function testBuildForm(): void
+    {
+        $form = new Registration();
+        $options = [];
+
+        /** @var FormBuilderInterface|\PHPUnit_Framework_MockObject_MockObject $builderMock */
+        $builderMock = $this->createMock(FormBuilderInterface::class);
+        $builderMock->expects(self::exactly(6))->method('add')->willReturnSelf();
+
+        $form->buildForm($builderMock, $options);
+    }
+}
