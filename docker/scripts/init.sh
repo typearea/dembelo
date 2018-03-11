db67412ea459537e2dba967092b0e4a70e70aa8b
Michael Giesler <michael@4horsemen.de>
2018-03-11 09:09:52 +0100

    issue #456: dockerfile will start encore watch now

--- docker/scripts/init.sh
+++ docker/scripts/init.sh
@@ -4,11 +4,10 @@ umask 0000
 
 cd /var/www/dembelo/www
 composer install
-yarn run encore dev
 bin/console assets:install web --symlink
 bin/console cache:warmup
 
 service nginx start
 service php7.1-fpm start
 
-tail -f /var/www/dembelo/www/var/logs/dev.log
+yarn run encore dev --watch
